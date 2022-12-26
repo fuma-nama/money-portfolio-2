9adf169a9df9176e1b304b65d684e4814f8e2919
SonMooSans <god63820869@gmail.com>
2022-12-26 21:47:42 +0800

    Add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
   server: {
     port: 3000,
   },
