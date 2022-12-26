5290cc5234da00e649e6ff3ed8e143deee02a1d8
SonMooSans <god63820869@gmail.com>
2022-12-26 23:57:29 +0800

    Improve navbar

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,33 +1,37 @@
 import { Navbar, Button, Text, Switch, Link } from "@nextui-org/react";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { useColorMode } from "../utils/theme";
 import { FiPhoneCall } from "react-icons/fi";
 import { links } from "../links";
+import { GiSharkFin } from "react-icons/gi";
+import { FaGithub } from "react-icons/fa";
 
 export function AppNavbar() {
   const { isDark, setTheme } = useColorMode();
   return (
     <Navbar isBordered variant="sticky" css={{ zIndex: "$10" }}>
-      <Navbar.Brand>
+      <Navbar.Brand css={{ gap: "$3" }}>
+        <Text as={GiSharkFin} />
         <Text b size="$lg">
           Money
         </Text>
       </Navbar.Brand>
-      <Navbar.Content>
-        <Navbar.Item hideIn="xs" as={Link} href={links.github} target="_blank">
-          <Button auto flat icon={<BsGithub />} color="secondary">
-            Github
-          </Button>
-        </Navbar.Item>
+      <Navbar.Content gap="$sm">
         <Navbar.Item hideIn="xs" as={Link} href="#contact">
           <Button auto flat color="secondary" icon={<FiPhoneCall />}>
             Contact
           </Button>
         </Navbar.Item>
+        <Navbar.Item hideIn="xs" as={Link} href={links.github} target="_blank">
+          <Button auto flat icon={<FaGithub />} color="secondary" />
+        </Navbar.Item>
         <Navbar.Item>
-          <Switch
-            checked={isDark}
-            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          <Button
+            auto
+            flat
+            color="secondary"
+            onClick={() => setTheme(isDark ? "light" : "dark")}
+            icon={isDark ? <BsMoonFill /> : <BsSunFill />}
           />
         </Navbar.Item>
       </Navbar.Content>
