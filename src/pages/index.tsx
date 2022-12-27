f6fa75bfb2351017a58d3cf447a82f6f2a52e59b
SonMooSans <god63820869@gmail.com>
2022-12-27 15:04:01 +0800

    Fix production bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import App from "./_app";
-import { NextPage } from "next";
 import { NextSeo } from "next-seo";
-type ServerSideProps = {};
 
-const IndexPage: NextPage<ServerSideProps> = () => (
-  <>
-    <NextSeo
-      title="Money's Portfolio"
-      description="The Portfolio of a Full-Stack Engineer"
-    />
-    <App />
-  </>
-);
-
-export default IndexPage;
+export default function IndexPage() {
+  return (
+    <>
+      <NextSeo
+        title="Money's Portfolio"
+        description="The Portfolio of a Full-Stack Engineer"
+      />
+      <App />
+    </>
+  );
+}
